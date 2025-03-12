f67cfbd21c0ecf47dca226f7963f85ef25140f6f
Maria Nicole Bulalacao <B00157129@mytudublin.ie>
2025-03-12 15:19:12 +0000

    Added C.py and s.py

---
+++ c.py
@@ -0,0 +1,147 @@
+from jsonrpcclient import request, parse, Ok
+import requests
+import base64
+import os
+
+
+def sendPing():
+    # send a request to the server
+    print("what is the servers port?")
+    
+    portToCall = int(input())
+    try:
+
+         response = requests.post(f"http://localhost:{portToCall}/", json=request("ping"))
+         parsed = parse(response.json())
+         print(parsed.result)
+
+    except:
+
+         print(" ----- Are you sure that port and function exist?")
+
+def sayHello(personName):
+    # send a request to the server
+
+    print("what is the servers port?")
+
+    portToCall = int(input())
+
+    try:
+        response = requests.post(f"http://localhost:{portToCall}/", json=request("printName", params={"name": personName}) )
+
+        parsed = parse(response.json())
+
+        print(parsed.result)
+
+    except:
+
+        print(" ------ Are you sure that port and function exist?")
+        
+def mkFolder():
+    print("What is the server's port?")
+    portToCall = int(input())
+    print("Enter folder name.")
+    folderName = input().strip()
+    
+    if not folderName:
+        folderName = "new_folder"    
+    try:
+        response = requests.post(f"http://localhost:{portToCall}/", json=request("makeFolder", params={"folder_name": folderName}) )
+        parsed = parse(response.json())
+        print(parsed.result)
+    except:
+        print("Are you sure that port exists?")
+
+def delFolder():
+    print("What is the server port?")
+    portToCall = int(input())
+    print("Enter folder name.")
+    folderName = input().strip()
+    
+    try:
+        response = requests.post(f"http://localhost:{portToCall}/", json=request("deleteFolder", params={"folder_name": folderName}) )
+        parsed = parse(response.json())
+        print(parsed.result)
+    except:
+        print("Are you sure that port exists?")
+
+def askWhoAreYou():
+    print("What is the server's port?")
+    portToCall = int(input())
+
+    try:
+        response = requests.post(f"http://localhost:{portToCall}/", json=request("whatServer") )
+        parsed = parse(response.json())
+        print(parsed.result)
+    except:
+        print("Are you sure that port exists?")
+
+def askVersion():
+    print("What is the server's port?")
+    portToCall = int(input())
+    
+    try:
+        response = requests.post(f"http://localhost:{portToCall}/", json=request("getVersion") )
+        parsed = parse(response.json())
+        print(parsed.result)
+    except:
+        print("Are your sure taht port exists?")
+
+def searchForFile():
+    print("What is the server's port?")
+    portToCall = int(input())
+    
+    print("Enter the filename to search for:")
+    filename = input().strip()
+    
+    try: 
+        response = requests.post(f"http://localhost:{portToCall}/", json=request("searchFile"), params={"filename": filename} )
+        parsed = parse(response.json())
+        print(parsed.result)
+    except:
+        print("Are you sure that port exists?")
+    
+
+        
+print("Welcome!")
+
+while True:
+    print("please type a menu option")
+    print("1. send ping")
+    print("2. say hello")
+    print("3. Make a folder")
+    print("4. Delete a Folder")
+    print("5. Who are you?")
+    print("6. Get server Python Version")
+    print("7. Search for a file")
+
+    option = input().strip()
+    
+    if option.isdigit():
+        option = int(option)
+    else:
+        print("Invalid input")
+
+    if option == 1:
+        sendPing()
+        
+    elif option == 2:
+        sayHello('john')
+        
+    elif option == 3:
+        mkFolder()
+        
+    elif option == 4:
+        delFolder()
+    
+    elif option == 5:
+        askWhoAreYou()
+    
+    elif option == 6:
+        askVersion()
+    
+    elif option == 7:
+        searchForFile()
+        
+    
+        
---
+++ s.py
@@ -0,0 +1,91 @@
+from jsonrpcserver import Success, method, serve
+import sys
+import os
+import subprocess
+
+if len(sys.argv) > 1:
+    serverNumber = sys.argv[1]
+else:
+    print("sorry you forgot to add a server number... shutting down")
+    quit()
+
+myFriends = list()
+
+@method
+def ping():
+    return Success("pong")
+
+@method
+def printName(name):
+
+    return Success(f"hello {name}")
+
+# method for making a folder
+@method 
+def makeFolder(folder_name):
+    try:
+        os.makedirs(folder_name, exist_ok=True)
+        return Success(f"Folder successfully made. ^-^")
+    except Exception as e:
+        return Success(f"Failed to make folder : {str(e)}")
+
+# method for deleting a folder
+@method 
+def deleteFolder(folder_name):
+    try:
+        os.rmdir(folder_name)
+        return Success(f"Folder successfully removed. ^-^")
+    except Exception as e:
+        return Success(f"Failed to remove folder : {str(e)}")
+
+# method for whoareyou - 
+# The server should return with thier number e.g 1,2,3 
+# and the port number they are running on
+@method
+def whatServer():
+    return Success(f"Server number {serverNumber} running on port {5000 + int(serverNumber)}")
+
+
+# method for get_version - Finding out what py ver is running on
+@method
+def getVersion():
+    try:
+        result = subprocess.run(["python3", "--version"], capture_output=True, text=True)
+        return Success(f"Python Version: {result.stdout}")
+    except Exception as e:
+        return Success(f"Failed to get Python Version : {str(e)}")
+
+# method for search XXXXX - 
+# The server should search for the filename the user has sent in the current directory
+@method
+def searchFile(filename):
+    if os.path.exists(filename):
+        return Success(f"File '{filename}' found in the directory.")
+    else:
+        return Success(f"File not found in the directory.")
+
+# method for startup X - if X = 2, a new server should start on port 5002
+
+# method for shutdown X - A message should be sent to the server that is running on port X to shutdown, where X is the last number added to the port,  500X.
+# This should also trigger an offline command as shown below.
+
+# method for list_friends - The current server should return a list of port numbers it knows other servers are running on. 
+# E.g., the server’s friends.
+
+# method for online X - When this command is received by a server, the server should attempt to tell all the other servers it is now online and they should add this port to their list of friends.
+
+# method for offline X - When this command is received by a server, it should tell all the other servers that the server on the defined port has shutdown and remove it from their list of friends.
+
+# method for heartbeat - When a client sends this command to a server, the server should ping all its server friends and return the result to the client.
+
+# method for pass msg X - msg is a text-based message.
+# x is the server number we want to send the message to.
+# When the client sends a pass command, it should go to the first server. If this server number does not match, it should pass the message on to the next server (one of his friends).
+  
+if __name__ == "__main__":
+
+    print(f"server number {serverNumber} running.....")
+
+    portNumber = '500' + serverNumber
+
+    serve(port=int(portNumber))
